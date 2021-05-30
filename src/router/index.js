b981e3d319e9e7dfd542b1007d3ba2526fae16fb
ferror <zmalcherczyk@gmail.com>
2021-05-30 19:46:17 +0200

    a little of code style

--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,29 @@ import Home from '@/views/Home.vue'
 import Services from '@/views/Services.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('@/views/About.vue')
-  },
-  {
-    path: '/services',
-    name: 'Services',
-    component: Services
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import('@/views/About.vue')
+    },
+    {
+        path: '/services',
+        name: 'Services',
+        component: Services
+    }
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: routes
+    history: createWebHistory(),
+    routes: routes
 })
 
 export default router
