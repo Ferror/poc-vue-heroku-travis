27f93de695e2db6719284af114edb0a723463fd7
Zbigniew Malcherczyk <zmalcherczyk@gmail.com>
2022-04-20 18:11:55 +0200

    Install Sentry (#1)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
+import * as Sentry from '@sentry/vue';
 
-createApp(App)
-    .use(router)
-    .mount('#app');
+const app = createApp(App).use(router);
+
+Sentry.init({
+    Vue: app,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    logErrors: true
+});
+
+app.mount('#app')
